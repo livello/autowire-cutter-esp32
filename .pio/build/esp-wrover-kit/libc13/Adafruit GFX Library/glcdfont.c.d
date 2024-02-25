1ca67b874551861c83d86350ba190586e681c31b
Sergey Konyukhov <livello@bk.ru>
2024-02-25 20:50:37 +0300

    fix reboot

---
+++ .pio/build/esp-wrover-kit/libc13/Adafruit GFX Library/glcdfont.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp-wrover-kit/libc13/Adafruit\ GFX\ Library/glcdfont.c.o: \
+ .pio/libdeps/esp-wrover-kit/Adafruit\ GFX\ Library/glcdfont.c
--- src/main.cpp
+++ src/main.cpp
@@ -103,22 +103,22 @@ void g_setup() {
 	oled.clear();
 	oled.update();
 
-	byte textPos1 = 8;
-	byte textPos2 = 32;
+	// byte textPos1 = 8;
+	// byte textPos2 = 32;
 
-	oled.createBuffer(5, 0, 66, textPos2 + 8 + 2);
+	// oled.createBuffer(5, 0, 66, textPos2 + 8 + 2);
 
-	oled.roundRect(5, textPos1 - 4, 65, textPos1 + 8 + 2, OLED_STROKE);
-	oled.setCursorXY(10, textPos1);
-	oled.print("SET MODE");
+	// oled.roundRect(5, textPos1 - 4, 65, textPos1 + 8 + 2, OLED_STROKE);
+	// oled.setCursorXY(10, textPos1);
+	// oled.print("SET MODE");
 
-	oled.roundRect(5, textPos2 - 4, 65, textPos2 + 8 + 2, OLED_FILL);
-	oled.setCursorXY(10, textPos2);
-	oled.invertText(true);
-	oled.print("LOL KEK");
+	// oled.roundRect(5, textPos2 - 4, 65, textPos2 + 8 + 2, OLED_FILL);
+	// oled.setCursorXY(10, textPos2);
+	// oled.invertText(true);
+	// oled.print("LOL KEK");
 
-	oled.sendBuffer();
-	oled.update();
+	// oled.sendBuffer();
+	// oled.update();
 }
 
 void setup() {
